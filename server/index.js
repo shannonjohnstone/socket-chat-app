89626ca3e2cecf41dabea55f81ada62f7523ca3e
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2017-11-05 22:51:53 +1100

    update structure and UI update project structure and build scripts to work better on dev and prod with more re use and for deployments to heroku update UI from plan index.html to using PUG templating engine

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('./config')
 import path from 'path'
 import http from 'http'
 import express from 'express'
+import pug from 'pug'
 import socketIO from 'socket.io'
 
 const publicPath = path.join(__dirname, '../public')
@@ -13,15 +14,29 @@ const io = socketIO(server)
 const PORT = process.env.PORT
 const NODE_ENV = process.env.NODE_ENV
 
-if (process.env.NODE_ENV !== 'production') app.use(express.static(`${publicPath}/local-dist`))
-else app.use(express.static(publicPath))
+app.set('views', path.join(__dirname, '../public', 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(publicPath))
+
+console.log(NODE_ENV, 'NODE_ENV');
+console.log(NODE_ENV === 'development', 'NODE_ENV === development');
+app.get('/', (req, res) => {
+  res.render('index.pug', {
+    isDevelopment: NODE_ENV === 'development',
+    title: 'Chat Application'
+  })
+})
+
+// if (process.env.NODE_ENV !== 'production') app.use(express.static(`${publicPath}/local-dist`))
+// else app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
   console.log('New user connected to chat server...');
 
   socket.on('createMessage', data => {
     const newData = { ...data, createAt: new Date() }
-    io.broadcast.emit('newMessage', newData) // this will emit any item emitted with `createMessage` to everyone connected at them time
+    socket.broadcast.emit('newMessage', newData) // this will emit any item emitted with `createMessage` to everyone connected at them time
   })
 })
 
