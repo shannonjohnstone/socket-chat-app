371eb7dbc6ea4386345b016f3ca3e9ced1f4957a
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2017-12-31 23:16:49 +1100

    update disable logic update logic so that the disalbe is only active when there location is trying to be sent and then not active afterwards or when it fails

--- client/js/geoLocationLib/index.js
+++ client/js/geoLocationLib/index.js
@@ -8,9 +8,13 @@ if (locationBtn !== null) {
   locationBtn.addEventListener('click', () => {
     if (!navigator.geolocation) return setPageReferral('Geolocation is not supported by your browser.')
 
+    locationBtn.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) =>{
       socket.emit(constants.CREATE_LOCATION_MESSAGE, { latitude, longitude })
-      locationBtn.setAttribute('disabled', 'disabled')
-    },() => setPageReferral('Unable to fetch location'))
+      locationBtn.removeAttribute('disabled')
+    }, () => {
+      locationBtn.removeAttribute('disabled')
+      setPageReferral('Unable to fetch location')
+    })
   })
 }
