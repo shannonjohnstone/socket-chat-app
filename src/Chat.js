fc78e2fa6073a4ff8fbad0ff6e54420fad243ead
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-01-11 21:31:41 +1100

    start porting across frontend logic

--- src/Chat.js
+++ src/Chat.js
@@ -1,27 +1,36 @@
-import React from 'react'
+/* global io */
+
+import React, { Component } from 'react'
 import { Button, Header, SideMenu, FormInputAddonBtn, ReferralBanner } from './components'
 
-const Home = () => {
-  return [
-    <SideMenu />,
-    <main className="l-main">
-      <Header
-        size="small"
-        position="fixed"
-        container={false}
-        navType="chat"
-      />
-      <ReferralBanner />
-      <section className="c-chat">
-        <div className="list c-chat__list" id="displayed-messages">
-          <div className="c-chat__buttons">
-            <FormInputAddonBtn />
-            <Button btnText='Location' />
+class Chat extends Component {
+  componentDidMount() {
+    this.socket = io();
+    this.socket.on('connect', () => console.log('Connected to server'))
+    this.socket.on('disconnect', () => console.log('Disconnect from server'))
+  }
+  render() {
+    return [
+      <SideMenu />,
+      <main className="l-main">
+        <Header
+          size="small"
+          position="fixed"
+          container={false}
+          navType="chat"
+        />
+        <ReferralBanner />
+        <section className="c-chat">
+          <div className="list c-chat__list" id="displayed-messages">
+            <div className="c-chat__buttons">
+              <FormInputAddonBtn />
+              <Button btnText='Location' />
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
-  ]
+        </section>
+      </main>
+    ]
+  }
 }
 
-export default Home;
+export default Chat;
