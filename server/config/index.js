810945811f8fc01932296f7f85815ea7b3a556f4
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-01-15 21:55:02 +1100

    clean up remove api server from project and rename some folders

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,4 @@
-import * as constants from '../../constants'
+import * as constants from './constants'
 const env = process.env.NODE_ENV || constants.DEVELOPMENT
 
 if (env === constants.DEVELOPMENT) {
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-import baseExampleRoute from './base-example'
-
-const routes = (app) => {
-  const baseUrl = '/api'
-  baseExampleRoute(app, baseUrl)
-}
-
-export default routes
